7ce728936129f31de755905a27ad7e1f2c0d74e2
Akniyeet <akniyet.maksatova@mail.ru>
2025-11-09 00:35:56 +0500

    Liquor.java

--- src/Liquor.java
+++ src/Liquor.java
@@ -1,17 +1,14 @@
 public class Liquor implements Visitable {
     private double price;
 
-    // Constructor to initialize the price
     Liquor(double item) {
         price = item;
     }
 
-    // Getter method to return the price
     public double getPrice() {
         return price;
     }
 
-    // Accept method for visitor pattern
     public double accept(Visitor visitor) {
         return visitor.visit(this);
     }
