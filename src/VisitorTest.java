081aaca7878299ccc9049e29c5a89fc93b6b8cc1
Akniyeet <akniyet.maksatova@mail.ru>
2025-11-09 00:37:05 +0500

    VisitorTest.java

--- src/VisitorTest.java
+++ src/VisitorTest.java
@@ -3,19 +3,16 @@ public class VisitorTest {
         TaxVisitor taxCalc = new TaxVisitor();
         TaxHolidayVisitor taxHolidayCalc = new TaxHolidayVisitor();
 
-        // Correct constructor usage for passing prices
         Necessity milk = new Necessity(3.47);
         Liquor vodka = new Liquor(11.99);
         Tobacco cigars = new Tobacco(19.99);
 
-        // Printing the prices with tax
         System.out.println(milk.accept(taxCalc) + "\n");
         System.out.println(vodka.accept(taxCalc) + "\n");
         System.out.println(cigars.accept(taxCalc) + "\n");
 
         System.out.println("TAX HOLIDAY PRICES\n");
 
-        // Printing the prices with holiday tax calculation
         System.out.println(milk.accept(taxHolidayCalc) + "\n");
         System.out.println(vodka.accept(taxHolidayCalc) + "\n");
         System.out.println(cigars.accept(taxHolidayCalc) + "\n");
