a025deaf68b11279ce42070b5d9addc4275ab394
Akniyeet <akniyet.maksatova@mail.ru>
2025-11-09 00:36:45 +0500

    Tobacco.java

--- src/Tobacco.java
+++ src/Tobacco.java
@@ -1,17 +1,14 @@
 public class Tobacco implements Visitable {
     private double price;
 
-    // Constructor to initialize the price
     Tobacco(double item) {
         price = item;
     }
 
-    // Getter method to return the price
     public double getPrice() {
         return price;
     }
 
-    // Accept method for visitor pattern
     public double accept(Visitor visitor) {
         return visitor.visit(this);
     }
